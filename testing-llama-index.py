0ec243bbc7ecffa87a2bc081fdd383a832af9483
soup-scott <campbellscott@tamu.edu>
2025-01-25 20:13:30 -0600

    added gitignore

--- testing-llama-index.py
+++ testing-llama-index.py
@@ -1,8 +1,9 @@
 import os
+from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
-os.environ["OPENAI_API_KEY"] = "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"
-
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 documents = SimpleDirectoryReader("./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
