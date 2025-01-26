e0006f9cab4a9b09e3b9cf2ee9e332078d3b8c54
soup-scott <campbellscott@tamu.edu>
2025-01-26 02:19:53 -0600

    completed json integration

--- testing-llama-index.py
+++ testing-llama-index.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = "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"
 documents = SimpleDirectoryReader("./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
