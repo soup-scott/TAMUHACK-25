c2241173b36f65d38d7e89386e0abcec50f44f6d
missle2 <108312727+missle2@users.noreply.github.com>
2025-01-26 02:18:21 -0600

    Added PDFs, updated llamaindex test

--- testing-llama-index.py
+++ testing-llama-index.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = "add it yourself bozo"
 documents = SimpleDirectoryReader("./data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-response = query_engine.query("What is the third article entail?")
+print(len(documents))
+response = query_engine.query("You are Clara, a toyota agent helping a customer pick a toyata car. Please introduce yourself to the customer, ask them their name and what they are looking for, and provide them with a list of the top 5 that fit their needs. Once the customer has selected a car, provide them with the price and transfer them to billing by saying \"Transferring you to billing now.\"")
 print(response)
+
+while response != "Transferring you to billing now.":
+    response = query_engine.query(input(response))
